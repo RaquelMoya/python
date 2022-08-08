f541cfa3a2acb5bdf2a154ffa662c7752a3084ff
unknown <rakelms90@gmail.com>
2022-08-08 14:44:21 +0200

    numeros

--- index.py
+++ index.py
@@ -7,10 +7,26 @@
 #print(hola)
 
 #print(type(hola))
-edad = int(input('Dime tu edad: '))
-print(edad)
 
-if edad > 18:
-    print('Eres mayor de edad')
-else:
-    print('No eres mayor de edad')
+
+
+
+#edad = int(input('Dime tu edad: '))
+#print(edad)
+#
+#if edad > 18:
+#    print('Eres mayor de edad')
+#else:
+#    print('No eres mayor de edad')
+
+
+
+numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+impares = []
+
+for num in numeros:
+    if num %2 == 1:
+        impares.append(num)
+       
+print(impares)
